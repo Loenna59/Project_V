eeab40fb1f02443f4ca4271fd8dcc9debd129f09
loenna <arterycutter@gmail.com>
2025-02-27 11:15:37 +0900

    arrow hit fx

--- Source/Project_V/Private/Player/Weapon/Arrow.cpp
+++ Source/Project_V/Private/Player/Weapon/Arrow.cpp
@@ -3,10 +3,28 @@
 
 #include "Player/Weapon/Arrow.h"
 
+#include "Kismet/GameplayStatics.h"
+#include "Particles/ParticleSystem.h"
+
 // Sets default values
 AArrow::AArrow()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = false;
+
+	ConstructorHelpers::FObjectFinder<UParticleSystem> tmpFx(TEXT("/Script/Engine.ParticleSystem'/Game/Assets/GoodFXImpact/FX/Particle/Chest_Point/Yellow/PS_GFXI_Yellow_Flare_04.PS_GFXI_Yellow_Flare_04'"));
+
+	if (tmpFx.Succeeded())
+	{
+		hitFx = tmpFx.Object;
+	}
+}
+
+void AArrow::OnComponentHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp,
+	FVector NormalImpulse, const FHitResult& Hit)
+{
+	Super::OnComponentHit(HitComponent, OtherActor, OtherComp, NormalImpulse, Hit);
+
+	UGameplayStatics::SpawnEmitterAtLocation(GetWorld(), hitFx, Hit.Location);
 }
    
